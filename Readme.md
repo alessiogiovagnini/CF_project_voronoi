fe0393a6f6a1eea622be53a9b1b2de96263c97ba
alessiogiovagnini <alessio.giovagnini@usi.ch>
2024-06-05 15:15:36 +0200

    cleaned a bit and improved readme

--- Readme.md
+++ Readme.md
@@ -1,5 +1,11 @@
+# Project for Computational Fabrication
+Project for the course "Computational Fabrication", This project create a voronoi
+structure from an input mesh
+
 # Requirements:
 - python 3.10.*
+
+Blender seem to require python 3.10 as right now (blender 4.0)
 
 ## set-up
 ```shell
@@ -15,4 +21,24 @@ pip install -r requirements.txt
 ```
 
 ## Usage
-TODO
+Running with the example:
+```shell
+python3.10 main.py
+```
+This will run the example.json file, it can be found inside the "data" folder.
+In the folder other than the json there are different stl files
+- dino_body.stl, dino_head.stl and dino_neck.stl are the sliced files
+- edited_original.stl is the original file with reduced geometry
+- out5.stl is the result of the run
+- edited_result.stl is a manual edited version I made as test
+
+The program can be used with a custom json file
+```shell
+python3.10 main.py --source <path to json file> --output <path to stl file>
+```
+The only requirement is that the structure is preserved following the example, 
+any number of sliced meshes can be included.
+The parameter "density" is the number of points per blender unit cube, so 
+the number should be adapted to the size of the mesh.
+The performance is not very good as the blender api is sequential, so I suggest
+to start with small numbers.
--- src/script.py
+++ src/script.py
@@ -12,52 +12,6 @@ import json
 from src.utils import import_file_stl, get_bounding_box, export_blend, clear_scene, export_stl_file, remove_objects
 
 
-def script_from_points(source: Path, output: Path, point_file: Path):
-    utils.clear_scene()
-    utils.import_file_obj(file=point_file)
-    objects_in_scene = bpy.data.objects
-    if len(objects_in_scene) > 1 or len(objects_in_scene) == 0:
-        print("imported file should contain 1 mesh only (for now)")
-        sys.exit(1)
-    points = utils.get_point_from_obj(obj=objects_in_scene[0])
-    voronoi = Voronoi(points=points)
-    voronoi_ridge_vertices = voronoi.ridge_vertices
-
-    # these two are needed to construct the geometry
-    voronoi_vertices: np.array = voronoi.vertices
-    voronoi_segments: list[tuple] = make_segments(ridge_vertices=voronoi_ridge_vertices)
-    print("made segments")
-    #min_distance, _, _ = utils.find_minimum_distance(voronoi_vertices)
-    radius: float = 0.01#min_distance/2
-    print("calculated radius")
-    utils.clear_scene()
-    print("starting making voronoi structure")
-    make_voronoi_structure(name="voronoi_structure", vertices=voronoi_vertices, segments=voronoi_segments, radius=radius)
-
-    utils.export_stl_file(output=output)
-
-
-# density is the number of point per 1 unit of volume
-def script_start(source: Path, output: Path, density: int = 100, radius: float = 1, points_file: Path = None):
-    utils.clear_scene()  # in case we have the default cube, camera and light
-
-    utils.import_file_stl(file=source)
-
-    # TODO for now the imported file should contain only one object/mesh
-    objects_in_scene = len(bpy.data.objects)
-    if objects_in_scene > 1 or objects_in_scene == 0:
-        print("imported file should contain 1 mesh only (for now)")
-        sys.exit(1)
-
-    b_box_min, b_box_max = utils.get_bounding_box(obj=bpy.data.objects[0])
-
-    x_size, y_size, z_size = utils.bounding_box_size(corner_min=b_box_min, corner_max=b_box_max)
-
-    volume = x_size * y_size * z_size
-
-    total_num_points: int = int(density * volume)
-
-
 def script_from_json(json_path: Path, out: Path):
     file = open(json_path)
     data = json.load(file)
